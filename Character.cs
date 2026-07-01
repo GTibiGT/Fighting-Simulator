7f2b11033029e924ff80f5b8fe67bbbbcab7f319
Ibrahim <164553372+GTibiGT@users.noreply.github.com>
2026-07-01 03:24:11 -0400

    Add files via upload
    added opponents and a basic game loop not fully functional yet almost there tho.

--- Character.cs
+++ Character.cs
@@ -5,10 +5,15 @@ namespace CharacterSelect
 {
     public class Character
     {
+        public static int Random_damage()
+        {
+            int dmg_chance = Random.Shared.Next(20, 41);
+            return dmg_chance;
+        }
         public string Name { get; set; } = "";
         public int Health { get; set; } = 100;
         public int Stamina { get; set; } = 100;
-        public Dictionary<int, string> Moves { get; set; } = new Dictionary<int, string>();
+        public Dictionary<int, Dictionary<string, List<int>>> Moves { get; set; } = new Dictionary<int, Dictionary<string, List<int>>>();
     }
 
     public class Boxer : Character
@@ -16,11 +21,15 @@ namespace CharacterSelect
         public Boxer()
         {
             Name = "Boxer";
-            Moves.Add(1, "Jab");
-            Moves.Add(2, "Cross");
-            Moves.Add(3, "Hook");
-            Moves.Add(4, "Uppercut");
-            Moves.Add(5, "Block");
+
+            Moves.Add(1, new Dictionary<string, List<int>> { { "Jab", new List<int> { 8, 15 } } });
+            Moves.Add(2, new Dictionary<string, List<int>> { { "Cross", new List<int> { 14, 24 } } });
+            Moves.Add(3, new Dictionary<string, List<int>> { { "Hook", new List<int> { 20, 32 } } });
+            Moves.Add(4, new Dictionary<string, List<int>> { { "Uppercut", new List<int> { 24, Random_damage() } } });
+            Moves.Add(5, new Dictionary<string, List<int>> { { "Block", new List<int> { 10, 0 } } });
+
+            Health = 100;
+            Stamina = 85;
         }
     }
 
@@ -29,11 +38,15 @@ namespace CharacterSelect
         public Wrestler()
         {
             Name = "Wrestler";
-            Moves.Add(1, "Takedown");
-            Moves.Add(2, "Suplex");
-            Moves.Add(3, "Body Slam");
-            Moves.Add(4, "Headlock");
-            Moves.Add(5, "Block");
+
+            Moves.Add(1, new Dictionary<string, List<int>> { { "Takedown", new List<int> { 14, 18 } } });
+            Moves.Add(2, new Dictionary<string, List<int>> { { "Suplex", new List<int> { 28, 38 } } });
+            Moves.Add(3, new Dictionary<string, List<int>> { { "Body Slam", new List<int> { 34, 45 } } });
+            Moves.Add(4, new Dictionary<string, List<int>> { { "Headlock", new List<int> { 18, 24 } } });
+            Moves.Add(5, new Dictionary<string, List<int>> { { "Block", new List<int> { 12, 0 } } });
+
+            Health = 85;
+            Stamina = 115;
         }
     }
 
@@ -42,11 +55,15 @@ namespace CharacterSelect
         public Kick_Boxer()
         {
             Name = "Kick-Boxer";
-            Moves.Add(1, "Jab");
-            Moves.Add(2, "Roundhouse Kick");
-            Moves.Add(3, "Front Kick");
-            Moves.Add(4, "Knee Strike");
-            Moves.Add(5, "Block");
+
+            Moves.Add(1, new Dictionary<string, List<int>> { { "Jab", new List<int> { 8, 14 } } });
+            Moves.Add(2, new Dictionary<string, List<int>> { { "Roundhouse Kick", new List<int> { 32, Random_damage() } } });
+            Moves.Add(3, new Dictionary<string, List<int>> { { "Front Kick", new List<int> { 22, 34 } } });
+            Moves.Add(4, new Dictionary<string, List<int>> { { "Knee Strike", new List<int> { 18, 28 } } });
+            Moves.Add(5, new Dictionary<string, List<int>> { { "Block", new List<int> { 10, 0 } } });
+
+            Health = 95;
+            Stamina = 90;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,37 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using CharacterSelect;
 
 class Program
 {
+    public static void PlayerTurn(Character player, Character enemy)
+    {
+        Console.WriteLine("Choose a move:");
+
+        foreach (KeyValuePair<int, Dictionary<string, List<int>>> move in player.Moves)
+        {
+            foreach (KeyValuePair<string, List<int>> attack in move.Value)
+            {
+                Console.WriteLine($"{move.Key}. {attack.Key} (Cost: {attack.Value[0]}, Damage: {attack.Value[1]})");
+            }
+        }
+
+        Console.Write("Choose a number (1-5): ");
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        // Perform move
+    }
+
+    public static void EnemyTurn(Character enemy, Character player)
+    {
+        Random rand = new Random();
+
+        int choice = rand.Next(1, 6);
+
+        // Perform move
+    }
+
     static void Main()
     {
         int num;
@@ -38,15 +66,58 @@ class Program
             p1 = new Kick_Boxer();
         }
 
+        Random random = new Random();
+
+        Character[] fighters =
+        {
+            new Boxer(),
+            new Wrestler(),
+            new Kick_Boxer()
+        };
+
+        Character p2 = fighters[random.Next(fighters.Length)];
+
         Console.WriteLine($"\nSo you chose: The {p1.Name}");
         Console.WriteLine($"Health: {p1.Health}");
         Console.WriteLine($"Stamina: {p1.Stamina}");
 
         Console.WriteLine("\nYour moves:");
 
-        foreach (KeyValuePair<int, string> move in p1.Moves)
+        foreach (KeyValuePair<int, Dictionary<string, List<int>>> move in p1.Moves)
         {
-            Console.WriteLine($"{move.Key}. {move.Value}");
+            foreach (KeyValuePair<string, List<int>> attack in move.Value)
+            {
+                Console.WriteLine($"{move.Key}. {attack.Key} (Cost: {attack.Value[0]}, Damage: {attack.Value[1]})");
+            }
+        }
+
+        Console.WriteLine("Get ready!");
+        Console.WriteLine("FIGHT!");
+
+        while (p1.Health > 0 && p2.Health > 0)
+        {
+            Console.Clear();
+            Console.WriteLine($"You are {p1.Name}: {p1.Health} HP | {p1.Stamina} Stamina");
+            Console.WriteLine($"Opponent is {p2.Name}: {p2.Health} HP | {p2.Stamina} Stamina");
+            Console.WriteLine();
+
+            // Player Turn
+            PlayerTurn(p1, p2);
+
+            if (p2.Health <= 0)
+                break;
+
+            // Enemy Turn
+            EnemyTurn(p2, p1);
+
+            if (p1.Health <= 0)
+                break;
+        }
+        if (p1.Health <= 0){
+            Console.WriteLine($"{p2.Name} Wins!");
+        }
+        else{
+            Console.WriteLine($"{p1.Name} Wins!");
         }
     }
 }
