c6f410e2a4879761d77aa7449d5ef4ecf2f7dd2b
Ibrahim <164553372+GTibiGT@users.noreply.github.com>
2026-07-01 02:26:48 -0400

    Add files via upload

--- Character.cs
+++ Character.cs
@@ -1,19 +1,52 @@
 using System;
 using System.Collections.Generic;
 
-namespace Character{
-    
-    public class Boxer{
-
+namespace CharacterSelect
+{
+    public class Character
+    {
+        public string Name { get; set; } = "";
+        public int Health { get; set; } = 100;
+        public int Stamina { get; set; } = 100;
+        public Dictionary<int, string> Moves { get; set; } = new Dictionary<int, string>();
     }
 
-    public class Wrestler{
-
+    public class Boxer : Character
+    {
+        public Boxer()
+        {
+            Name = "Boxer";
+            Moves.Add(1, "Jab");
+            Moves.Add(2, "Cross");
+            Moves.Add(3, "Hook");
+            Moves.Add(4, "Uppercut");
+            Moves.Add(5, "Block");
+        }
     }
 
-    public class Kick_Boxer{
-
+    public class Wrestler : Character
+    {
+        public Wrestler()
+        {
+            Name = "Wrestler";
+            Moves.Add(1, "Takedown");
+            Moves.Add(2, "Suplex");
+            Moves.Add(3, "Body Slam");
+            Moves.Add(4, "Headlock");
+            Moves.Add(5, "Block");
+        }
     }
-    
 
+    public class Kick_Boxer : Character
+    {
+        public Kick_Boxer()
+        {
+            Name = "Kick-Boxer";
+            Moves.Add(1, "Jab");
+            Moves.Add(2, "Roundhouse Kick");
+            Moves.Add(3, "Front Kick");
+            Moves.Add(4, "Knee Strike");
+            Moves.Add(5, "Block");
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,46 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
 using System;
-using System.Globalization;
-using Character;
+using System.Collections.Generic;
+using CharacterSelect;
 
-class Sim{
-    static void Main(){
-        bool isValid = false;
+class Program
+{
+    static void Main()
+    {
         int num;
-        Console.WriteLine("Welcome to Fighting Simulator this is a turn based game where each character");
-        Console.WriteLine("has their own specific skill/martial art and weaknesses and strengths.");
-        Console.WriteLine("");
-        Console.WriteLine("Choose your character");
+
+        Console.WriteLine("Welcome to *Fighting Simulator*!");
+        Console.WriteLine("This is a turn-based game where each character has their own skills.\n");
+
+        Console.WriteLine("Choose your character:");
         Console.WriteLine("1. Boxer");
         Console.WriteLine("2. Wrestler");
         Console.WriteLine("3. Kick-Boxer");
-        Console.WriteLine("Enter corresponding number to the style you want to pick");
-        do
+
+        Console.Write("\nEnter the number corresponding to the style you want to pick: ");
+
+        while (!int.TryParse(Console.ReadLine() ?? "", out num) || num < 1 || num > 3)
         {
-            string input = Console.ReadLine();
-            isValid = int.TryParse(input, out num);
-
-            if (!isValid || num < 1 || num > 3)
-            {
-                Console.WriteLine("Invalid input. Please enter a valid integer between 1 and 3.");
-            }
-
-        } while (!isValid || num < 1 || num > 3);
-    
-        Console.WriteLine("So you chose: The " + charSelect(num));
-    }
-
-    static string charSelect(int num){
-        Dictionary<int, string> chars = new Dictionary<int, string>
-        {
-            {1, "Boxer"},
-            {2, "Wrestler"},
-            {3, "Kick-Boxer"}
-        };
-        if (chars.ContainsKey(num)){
-            return chars[num];
+            Console.Write("Invalid input. Enter 1, 2, or 3: ");
         }
-        return "Unknown";
-    
+
+        Character p1;
+
+        if (num == 1)
+        {
+            p1 = new Boxer();
+        }
+        else if (num == 2)
+        {
+            p1 = new Wrestler();
+        }
+        else
+        {
+            p1 = new Kick_Boxer();
+        }
+
+        Console.WriteLine($"\nSo you chose: The {p1.Name}");
+        Console.WriteLine($"Health: {p1.Health}");
+        Console.WriteLine($"Stamina: {p1.Stamina}");
+
+        Console.WriteLine("\nYour moves:");
+
+        foreach (KeyValuePair<int, string> move in p1.Moves)
+        {
+            Console.WriteLine($"{move.Key}. {move.Value}");
+        }
     }
 }
